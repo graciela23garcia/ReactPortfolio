b932d153b6f26434b6bcc4f4d8b09f3e1408ff1f
graciela23garcia <70188756+graciela23garcia@users.noreply.github.com>
2021-02-26 14:47:21 -0800

    changing styling and adding links

---
+++ reactportfolio/src/resumeData.js
@@ -0,0 +1,99 @@
+let resumeData = {
+  "imagebaseurl": "https://github.com/graciela23garcia",
+  "name": "Graciela Garcia",
+  "role": "Full Stack Software Developer",
+  "linkedinId": "Your LinkedIn Id",
+  "skypeid": "Your skypeid",
+  "roleDescription": " I am a graduate from the program of the University of Washington. I enjoy exploring in various parts of both front end and back end web development. Below you will find links to my Linkedin & Github and profiles. There, you will find numerus projects that I have completed in the course of this program. ",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/graciela-garcia-2298291b3/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/graciela23garcia",
+      "className": "fa fa-github"
+    },
+  ],
+  "aboutme": "After completing this program I intend to further my knowledge in data structures and more react-base libraries that will ensure my continuing success in this field.  ",
+  "address": "Spokane, WA",
+  "website": "graciela@gmail.com",
+  "education": [
+    {
+      "UniversityName": "University of Washington",
+      "specialization": "Full Stack Software Development",
+      "MonthOfPassing": "Feb",
+      "YearOfPassing": "2021",
+      "Achievements": "Completed"
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "Leggari",
+      "specialization": "LMS Production",
+      "MonthOfLeaving": "Jan",
+      "YearOfLeaving": "2018",
+      "Achievements": "Currently Employment"
+    }
+  ],
+  "skillsDescription": "Developing Languages/Technologies",
+  "skills": [
+    {
+      "skillname": "HTML"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "NODEJS"
+    },
+    {
+      "skillname": "MYSQL"
+    },
+    {
+      "skillname": "MONGODB"
+    },
+    {
+      "skillname": "EXPRESS"
+    },
+    {
+      "skillname": "Reactjs"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "project1",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    },
+    {
+      "name": "project2",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project.jpg"
+    },
+    {
+      "name": "project3",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project2.png"
+    },
+    {
+      "name": "project4",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
